3567f75d2b45218d1c155b5d98c66878655118c5
Martin Doucha <mdoucha@suse.cz>
2026-04-21 15:58:56 +0200

    install_ltp: Lock kernel-default unconditionally

--- tests/kernel/install_ltp.pm
+++ tests/kernel/install_ltp.pm
@@ -316,7 +316,7 @@ sub run {
 
     # Lock kernel default on transactional system, RT flavors and MinimalVM
     # This is workaround for poo#165036 to prevent kernel-default and kernel-default-base installation
-    zypper_call("al kernel-default kernel-default-base") if (is_transactional && (get_var('FLAVOR', '') =~ /Base-RT-Updates|Base-RT|Base-RT-encrypted|Base-Kernel-RT/) || is_jeos);
+    zypper_call("al kernel-default kernel-default-base");
 
     # Register Extras repository on SL Micro 6.0+
     add_suseconnect_product('SL-Micro-Extras', get_var('VERSION')) if (is_sle_micro('6.0+'));
